9bfa16f13e94619dfc7f5af4aa277d4f7f7222f5
Daniel Gordon <danielgordon10@gmail.com>
2018-07-02 00:20:37 -0700

    checkpointing

--- remove_pingendo.py
+++ remove_pingendo.py
@@ -1,5 +1,6 @@
 import glob
 import os
+from datetime import datetime
 folders = [ff for ff in glob.glob('*') if os.path.isdir(ff)]
 files = glob.glob('*.html')
 while len(folders) > 0:
@@ -12,11 +13,22 @@ for file in files:
     lines = [line for line in open(file)]
     new_lines = []
     for line in lines:
-        if not ('<pingendo' in line or
+        if ('<pingendo' in line or
                 '</pingendo>' in line or
                 'Pingendo_logo' in line):
-            new_lines.append(line)
+            continue
+        elif 'Last Updated' in line:
+            import pdb
+            #pdb.set_trace()
+            start_padding = line[:len(line) - len(line.lstrip())]
+            line = start_padding + '<p>&copy; 2018 Daniel Gordon. Last Updated ' + datetime.now().strftime('%B') + ' ' + str(datetime.now().year) + '.</p>\n'
+        elif line == '        </div>    </div>':
+            line = '         </div>\n    </div>'
+
+        new_lines.append(line)
+
     new_file = open(file, 'w')
     new_file.write(''.join(new_lines))
     new_file.close()
+    print('Updated', file)
         
