3fc0650046626ca3cb765cc311357dae344dd825
Daniel Gordon <danielgordon10@gmail.com>
2020-02-06 18:58:56 -0800

    update

--- export_website.sh
+++ export_website.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 python remove_pingendo.py
 chmod -R +r *
 #scp -r * xkcd@tricycle.cs.washington.edu:/cse/web/homes/xkcd/
 #rsync -avW * xkcd@tricycle.cs.washington.edu:/cse/web/homes/xkcd
+git add -A
+git commit -m "update"
+git push github master
 
 
