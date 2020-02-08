29cf2b287d569672f869c32c6bd908f84caff755
Daniel Gordon <xkcd@cs.washington.edu>
2020-02-07 18:53:02 -0800

    update

--- export_website.sh
+++ export_website.sh
@@ -3,9 +3,9 @@
 python remove_pingendo.py
 chmod -R +r *
 #scp -r * xkcd@tricycle.cs.washington.edu:/cse/web/homes/xkcd/
-rsync -avW * xkcd@tricycle.cs.washington.edu:/cse/web/homes/xkcd
-#git add -A
-#git commit -m "update"
-#git push github master
+#rsync -avW * xkcd@tricycle.cs.washington.edu:/cse/web/homes/xkcd
+git add -A
+git commit -m "update"
+git push github master
 
 
