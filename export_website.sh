6fde011a973c5fdfaa6d2e31e372c8364b6158a4
Daniel Gordon <danielgordon10@gmail.com>
2020-02-18 12:04:05 -0800

    update

--- export_website.sh
+++ export_website.sh
@@ -3,6 +3,8 @@
 python make_templates_index.py
 python remove_pingendo.py
 chmod -R +r *
+chmod 700 old
+chmod 700 cv.docx
 #scp -r * xkcd@tricycle.cs.washington.edu:/cse/web/homes/xkcd/
 rsync -avW * xkcd@bicycle.cs.washington.edu:/cse/web/homes/xkcd --exclude *.swp
 #git add -A
