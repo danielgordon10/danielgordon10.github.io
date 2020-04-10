5ac1d510b8a34ab4468c96eda4133490d19e0ba1
Daniel Gordon <danielgordon10@gmail.com>
2020-04-09 17:39:38 -0700

    update

--- assets/js/navbar-ontop.js
+++ assets/js/navbar-ontop.js
@@ -8,37 +8,40 @@
     var scripts= document.getElementsByTagName('script');
     var path = scripts[scripts.length-1].src.split('?')[0];      // remove any ?query
     var homeDir = path.split('/').slice(0, -1).join('/') + '/../../';  // remove last filename part of path
-    document.write("<style id='temp-navbar-ontop'>.navbar {opacity:0; transition: none !important}</style>")
+    document.write("<style id='temp-navbar-ontop'>.navbar {opacity:0; transition: none !important}</style>");
 
-    var className = "navbar-ontop"
+    var className = "navbar-ontop";
     // we start hidden, to avoid flickering
 
     function update() {
         // toggle className based on the scrollTop property of document
-        var nav = document.querySelector(".navbar")
+        var nav = document.querySelector(".navbar");
 
-        if (window.scrollY > 15)
-            nav.classList.remove(className)
-        else
-            nav.classList.add(className)
+        if (window.scrollY > 15) {
+            nav.classList.remove(className);
+        } else {
+            nav.classList.add(className);
+        }
     }
     $(document).ready(function() {
         $("#nav-placeholder").load(homeDir + "nav.html", function() {
             $(window).on('show.bs.collapse', function (e) {
                 $(e.target).closest("." + className).removeClass(className);
-            })
+            });
 
             $(window).on('hidden.bs.collapse', function (e) {
-                update()
-            })
-            update()
+                update();
+            });
+            update();
             // still hacking to avoid flickering
             setTimeout(function() {
-                document.querySelector("#temp-navbar-ontop").remove()
-            })
+                document.querySelector("#temp-navbar-ontop").remove();
+            });
+            var nav = document.querySelector(".navbar");
+            nav.style.transition.length = 0.5;
         });
         window.addEventListener("scroll", function() {
-            update()
-        })
+            update();
+        });
     });
 })();
