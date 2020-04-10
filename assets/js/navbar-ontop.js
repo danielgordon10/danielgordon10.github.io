093e9c27f07cc9e12a16401e60d8703af1ae948e
Daniel Gordon <danielgordon10@gmail.com>
2020-04-10 14:34:16 -0700

    update

--- assets/js/navbar-ontop.js
+++ assets/js/navbar-ontop.js
@@ -8,10 +8,8 @@
     var scripts= document.getElementsByTagName('script');
     var path = scripts[scripts.length-1].src.split('?')[0];      // remove any ?query
     var homeDir = path.split('/').slice(0, -1).join('/') + '/../../';  // remove last filename part of path
-    document.write("<style id='temp-navbar-ontop'>.navbar {opacity:0; transition: none !important}</style>");
 
     var className = "navbar-ontop";
-    // we start hidden, to avoid flickering
 
     function update() {
         // toggle className based on the scrollTop property of document
@@ -33,12 +31,7 @@
                 update();
             });
             update();
-            // still hacking to avoid flickering
-            setTimeout(function() {
-                document.querySelector("#temp-navbar-ontop").remove();
-            });
             var nav = document.querySelector(".navbar");
-            nav.style.opacity = 1;
             nav.style.transitionDuration = "0.5s";
             nav.style.webkitTransitionDuration = "0.5s";
         });
