a44addb89298aece097c4a6128f75a0fbf461938
Daniel Gordon <danielgordon10@gmail.com>
2020-06-27 15:08:31 -0700

    update

--- export_website.sh
+++ export_website.sh
@@ -6,10 +6,9 @@ python fix_author_spacing.py
 chmod -R +r *
 chmod 700 cv.docx
 #scp -r * xkcd@tricycle.cs.washington.edu:/cse/web/homes/xkcd/
-#rsync -avW --info=progress2 * xkcd@bicycle.cs.washington.edu:/cse/web/homes/xkcd --exclude *.swp --exclude .git
+rsync -avW --info=progress2 * xkcd@bicycle.cs.washington.edu:/cse/web/homes/xkcd --exclude *.swp --exclude .git
 git add -A
 git commit -m "update"
 git push github master
-
 
 
